001a07d10525a102f60ab46986e905f3ba8c2bb5
dof-man <onucheojofriday@gmail.com>
2025-08-31 23:46:48 +0100

    mobile4

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -7,10 +7,10 @@ export default function Index() {
       
 <View>
   <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross 
+    platforms</Text>
   <Text style={styles.smallText}>ALX is awesome</Text>
 </View>
-
 
 </View>
   );
