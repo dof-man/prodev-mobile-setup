46a0414b26566ea52bad408b88cc8e0b692a7cb4
dof-man <onucheojofriday@gmail.com>
2025-08-31 23:42:15 +0100

    mobile4

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -10,6 +10,8 @@ export default function Index() {
   <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
   <Text style={styles.smallText}>ALX is awesome</Text>
 </View>
+
+
 </View>
   );
 }
